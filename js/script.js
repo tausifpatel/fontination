114c853707894ed5279d4b514f8bae3d929a8d85
tausifpatel <31739633+tausifpatel@users.noreply.github.com>
2018-04-30 19:18:49 +0100

    Uploading Fontination

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// document.querySelectorAll('#head').addEventListener("click", editable);
+
+// function editable(){
+// 	document.querySelector('#head').contentEditable = true;
+// }
+
+
+// let el = document.querySelectorAll('h1');
+
+// for(var i=0; i < el.length; i++){
+//     var memory = el[i];
+//     memory.addEventListener('click',function(){
+//     	memory.setAttribute('contenteditable',true);
+//     });
+// }
+
