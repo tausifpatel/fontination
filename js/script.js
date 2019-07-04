ab3d5853036baaaab757c95e9f10029ab6d192f4
Tausif Patel <rrraja@hotmail.co.uk>
2019-07-04 08:52:11 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,5 @@
-// document.querySelectorAll('#head').addEventListener("click", editable);
-
-// function editable(){
-// 	document.querySelector('#head').contentEditable = true;
-// }
-
-
-// let el = document.querySelectorAll('h1');
-
-// for(var i=0; i < el.length; i++){
-//     var memory = el[i];
-//     memory.addEventListener('click',function(){
-//     	memory.setAttribute('contenteditable',true);
-//     });
-// }
-
-// Add event listener
-// run loop
-// compare if it is required ID
-// select query to edit the content
-
-
 document.addEventListener('click', function(event){
-  for(var i = 0; i < 3; i++){
+  for(var i = 0; i < 20; i++){
     if(parseInt(event.target.id) === i){
       document.getElementById(i).contentEditable = true;
     }
