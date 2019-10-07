1984fc25b666fa30d146a7516587ced395b549c5
Tausif Patel <rrraja@hotmail.co.uk>
2019-10-08 00:34:33 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
 document.addEventListener('click', function(event){
-  for(var i = 0; i < 20; i++){
-    if(parseInt(event.target.id) === i){
-      document.getElementById(i).contentEditable = true;
-    }
-  }
+  document.getElementById(parseInt(event.target.id)).contentEditable = true;
 });
